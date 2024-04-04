5dfebb769426d77fa5c920a1bd0f6b8cb86d1387
Erick Mutisya <ErickM@C1-S.LOCAL>
2024-04-04 14:59:37 +0300

    Initial Commit

---
+++ scripts/typed.js
@@ -0,0 +1,13 @@
+// My JS code
+
+var typed = new Typed('#typed', {
+  strings: [
+      'Cyber security specialist',
+      'Penetration Tester',
+      'Python Software Developer'
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
